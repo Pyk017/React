3954dbbd73bf208c22764c11cbcb56933342629e
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2022-05-26 20:41:41 +0530

    26-05-2022

---
+++ react-blog/src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return (
+    <div className="create-blog">
+      <h2>Add a new Blog!</h2>
+    </div>
+  );
+};
+
+export default Create;
--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,37 +1,18 @@
-import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  const [isPending, setIsPending] = useState(true);
-
-  const [error, setError] = useState(null);
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   // const handleDeleteBlog = (id) => {
   //   const newBlogs = blogs.filter((blog) => blog.id !== id);
   //   setBlogs(newBlogs);
   // };
   // handleDeleteBlog={handleDeleteBlog}
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data for that resource.");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err.message);
-      });
-  }, []);
 
   return (
     <div className="home">
