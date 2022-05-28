5a878b856b3238305523d282c2ebdff0960fdf52
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2022-05-28 12:54:23 +0530

    28-05-2022

--- react-blog/src/BlogDetails.js
+++ react-blog/src/BlogDetails.js
@@ -1,8 +1,12 @@
-import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+
+  const navigate = useNavigate();
 
   const {
     data: blog,
@@ -10,11 +14,18 @@ const BlogDetails = () => {
     error,
   } = useFetch("http://localhost:8000/blogs/" + id);
 
+  const [isPendingDeletion, setIsPending] = useState(false);
+
   const deleteBlog = () => {
+    setIsPending(true);
+
     fetch("http://localhost:8000/blogs/" + id, { method: "DELETE" })
-      .then(() => console.log("Blog deleted successfully"))
+      .then(() => {
+        console.log("Blog Deleted Successfully!");
+        setIsPending(false);
+        navigate("/");
+      })
       .catch((err) => console.log("Error while deleting blog ", err));
-    window.location.replace("/");
   };
 
   return (
@@ -26,12 +37,27 @@ const BlogDetails = () => {
           <h2> {blog.title}</h2>
           <small>{blog.likes} Likes</small>
           <p>{blog.body}</p>
-          <button className="deleteBtn" onClick={deleteBlog}>
-            Delete
-          </button>
-          <button className="backBtn">
-            <Link to="/">Back</Link>
-          </button>
+          {!isPendingDeletion && (
+            <>
+              <button className="deleteBtn" onClick={deleteBlog}>
+                Delete
+              </button>
+              <button className="backBtn" onClick={() => navigate("/")}>
+                Back
+              </button>
+            </>
+          )}
+
+          {isPendingDeletion && (
+            <>
+              <button className="deleteBtn" disabled>
+                Deleting Blog ...
+              </button>
+              <button className="backBtn" disabled>
+                Back
+              </button>
+            </>
+          )}
         </article>
       )}
     </div>
--- react-blog/src/Create.js
+++ react-blog/src/Create.js
@@ -1,7 +1,65 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+
+  const [body, setBody] = useState("");
+
+  const [isPending, setIsPending] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const blogObject = {
+      title,
+      body,
+      likes: 0,
+    };
+
+    setIsPending(true);
+    // console.log(blogObject);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blogObject),
+    })
+      .then(() => {
+        console.log("New Blog added");
+        setIsPending(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setIsPending(false);
+      });
+  };
+
   return (
     <div className="create-blog">
       <h2>Add a new Blog!</h2>
+      <form onSubmit={handleSubmit}>
+        <label> Blog Title :- </label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label> Blog Body :- </label>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          cols="30"
+          rows="10"
+          required
+        />
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
+      </form>
     </div>
   );
 };
