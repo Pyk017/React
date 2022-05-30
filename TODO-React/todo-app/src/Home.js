14a049e572cd8cf6db47952a00c8abb56a7ebe9f
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2022-05-30 23:01:09 +0530

    30-05-2022

--- TODO-React/todo-app/src/Home.js
+++ TODO-React/todo-app/src/Home.js
@@ -1,7 +1,43 @@
 const Home = () => {
   return (
     <div className="home-component">
-      <h2>Home Page</h2>
+      <div className="todo-comp header">
+        <h3>TODO List</h3>
+      </div>
+      <div className="todo-comp searchbox">
+        <input
+          type="text"
+          className="search-list"
+          placeholder="Search Task ..."
+        />
+        <div className="search-icon"></div>
+      </div>
+      <div className="todo-comp task-list">
+        <div className="tasks">
+          <div className="listing-icon">Listing Icon</div>
+          <div className="task-name">Task 1</div>
+          <div className="mark-as-done-icon">mark-as-done icon</div>
+          <div className="delete-icon">delete-icon</div>
+        </div>
+        <div className="tasks">
+          <div className="listing-icon">Listing Icon</div>
+          <div className="task-name">Task 1</div>
+          <div className="mark-as-done-icon">mark-as-done icon</div>
+          <div className="delete-icon">delete-icon</div>
+        </div>
+        <div className="tasks">
+          <div className="listing-icon">Listing Icon</div>
+          <div className="task-name">Task 1</div>
+          <div className="mark-as-done-icon">mark-as-done icon</div>
+          <div className="delete-icon">delete-icon</div>
+        </div>
+        <div className="tasks">
+          <div className="listing-icon">Listing Icon</div>
+          <div className="task-name">Task 1</div>
+          <div className="mark-as-done-icon">mark-as-done icon</div>
+          <div className="delete-icon">delete-icon</div>
+        </div>
+      </div>
     </div>
   );
 };
