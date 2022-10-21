0e4fedc4d93f78a798f11e8994a82645b0036ebd
Prakhar Kumar Kashyap <prakharkumar506978@gmail.com>
2022-10-21 20:58:53 +0530

    21-10-2022

---
+++ react-skeleton-screen/src/App.js
@@ -0,0 +1,18 @@
+import User from "./components/User";
+import Articles from "./components/Articles";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>React Skeletons</h1>
+      </header>
+      <div className="content">
+        <Articles />
+        <User />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-skeleton-screen/src/skeletons/SkeletonArticle.js
@@ -0,0 +1,21 @@
+import SkeletonElements from "./SkeletonElements";
+
+import Shimmer from "./Shimmer";
+
+const SkeletonArticle = ({ theme }) => {
+  const themeClass = theme || "light";
+
+  return (
+    <div className={`skeleton-wrapper ${themeClass}`}>
+      <div className="skeleton-article">
+        <SkeletonElements type="title" />
+        <SkeletonElements type="text" />
+        <SkeletonElements type="text" />
+        <SkeletonElements type="text" />
+      </div>
+      <Shimmer />
+    </div>
+  );
+};
+
+export default SkeletonArticle;
